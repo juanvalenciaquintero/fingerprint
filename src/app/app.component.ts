02625e9d8395064c78e3f6a4f610855ac03530cd
juan140269@gmail.com <juan140269@gmail.com>
2020-08-10 13:37:11 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AndroidFingerprintAuth } from '@ionic-native/android-fingerprint-auth/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router,
+    public androidFingerprintAuth: AndroidFingerprintAuth
   ) {
     this.initializeApp();
   }
@@ -22,6 +26,45 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.androidFingerprintAuth.isAvailable().then(
+        (result) =>
+        {
+          if (result.isAvailable)
+          {
+            this.androidFingerprintAuth.encrypt({ clientId: "clienteid", username: "username", password: "password" }).then((result) =>
+            {
+              if (result.withFingerprint)
+              {
+                alert("Successfully authenticated with fingerprint");
+                alert("Encrypt credentials: " + result.token);
+                this.router.navigate(['/inicio']);
+              } else if (result.withBackup)
+              {
+                alert("Succesfully authenticated with backup password");
+                this.router.navigate(['/inicio']);
+              } else
+              {
+               alert("Didnot authenticate!");
+              }
+            }, (err) =>
+            {
+                if (err === this.androidFingerprintAuth.ERRORS.FINGERPRINT_CANCELLED)
+                {
+                  alert("Fingerprint authentication cancelled");
+                }
+                else
+                {
+                  alert(JSON.stringify(err));
+                }
+            })
+          } else
+          {
+            alert("Fingerprint authentication not available");
+          }
+        })
+    }, (err) =>
+    {
+        alert(JSON.stringify(err));
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AndroidFingerprintAuth } from '@ionic-native/android-fingerprint-auth/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AndroidFingerprintAuth
   ],
   bootstrap: [AppComponent]
 })
